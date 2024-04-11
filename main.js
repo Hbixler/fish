72967017d21efba2e0cc557cbc17f11d99f2bf7e
kai-h-8 <kai.yy.houston@gmail.com>
2024-04-11 00:52:54 -0400

    A strong start

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("I am a fish")
+console.log("I am a fish");
+
+let numGoldfish = 0;
+let goldfishSpan = document.getElementById('numGoldfish');
+goldfishSpan.innerText = numGoldfish;
+
+function goFishing() {
+    // console.log(numGoldfish);
+    numGoldfish ++;
+    goldfishSpan.innerText = numGoldfish;
+}
