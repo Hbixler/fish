0edb568e8c7cd911c01d2e991b0a2cf7035f2431
Hbixler <hbixler@unomaha.edu>
2024-05-02 22:14:33 -0500

    Created many visibility functions and corrected Kai's spelling of the word visibility

--- trading.js
+++ trading.js
@@ -1,38 +1,89 @@
+// Check visibility
+function isTradingSectionVisible() {
+    let tradingDiv = document.getElementById('trading-div'); // make trading section visible
+    return tradingDiv.style.visibility == 'visible';
+}
+function makeTradingSectionVisible() {
+    let tradingDiv = document.getElementById('trading-div'); // make trading section visible
+    tradingDiv.style.visibility = 'visible';
+
+    let sellFishHeadingDiv = document.getElementById('sellFishHeading'); // make sell fish heading visible
+    sellFishHeadingDiv.style.visibility = 'visible';
+}
+function isFishTradingVisible(fishNumber) {
+    let fishTradeDiv = document.getElementById("fishTrading" + fishNumber + "-div"); // add fish to the trading section
+    return fishTradeDiv.style.visibility === 'visible';
+}
+function makeFishTradingVisible(fishNumber) {
+    let fishTradeDiv = document.getElementById("fishTrading" + fishNumber + "-div"); // add fish to the trading section
+    fishTradeDiv.style.visibility = 'visible';
+}
+function isEquipmentTradingVisible() {
+    fishingRodHeading = document.getElementById('equipment-heading');
+    return fishingRodHeading.style.visibility == 'visible';
+}
+function makeEquipmentTradingVisible() {
+    let fishingRodHeading = document.getElementById('equipment-heading');
+    fishingRodHeading.style.visibility = 'visible';
+    
+    let fishingRodTradingDiv = document.getElementById('equipment-div');
+    fishingRodTradingDiv.style.removeProperty('border');
+}
+function makeRodVisible(rodNum) {
+    let fishingRodTrading = document.getElementById('fishingRodTrading' + rodNum + '-div')
+    fishingRodTrading.style.visibility = 'visible';
+
+    let fishingRodButton = document.getElementById('buyRod' + rodNum);
+    fishingRodButton.style.visibility = 'visible';
+}
+function makeBaitTradingVisible() {
+    baitHeadingDiv = document.getElementById('bait-heading');
+    baitHeadingDiv.style.visibility = 'visible';
+    
+    baitDiv = document.getElementById("bait-div");
+    baitDiv.style.removeProperty('border');
+}
+function makeHabitatTradingVisible() {
+    let habitatTradingHeading = document.getElementById('habitat-heading')
+    habitatTradingHeading.style.visibility = 'visible';
+
+    let habitatTradingDiv = document.getElementById('habitat-trading-div');
+    habitatTradingDiv.style.removeProperty('border'); 
+}
+function makeHabitatVisible(habitatNum) {
+    let fishingHabitatTrading = document.getElementById('habitatTrading' + habitatNum + '-div');
+    fishingHabitatTrading.style.visibility = 'visible';
+
+    let fishingHabitatButton = document.getElementById('buyHabitat' + habitatNum);
+    fishingHabitatButton.style.visibility = 'visible';
+
+    fishHabitats[habitatNum].unlocked = true;
+}
+function isVehicleSectionVisible() {
+    vehiclesDiv = document.getElementById('vehicles-div'); // displays vehicles
+    return vehiclesDiv.style.visibility == 'visible';
+}
+function makeVehicleSectionVisible() {
+    vehiclesDiv = document.getElementById('vehicles-div'); // displays vehicles
+    vehiclesDiv.style.visibility == 'visible';
+}
+
 // Sand dollar count
 function updateSandDollars() {
     let sandDollarsSpan = document.getElementById('sandDollars');
     sandDollarsSpan.innerText = Math.round(sandDollars * 100) / 100;
 
-    if(sandDollars > 10 && !fishingRods[0].unlocked) { // unlocks equipment section of trading when 10 sanddollars are earned
-        fishingRodTrading0 = document.getElementById('fishingRodTrading0-div')
-        fishingRodTrading0.style.visibility = 'visible';
+    if(sandDollars > 10 && !isEquipmentTradingVisible()) { // unlocks equipment section of trading when 10 sanddollars are earned
 
-        fishingRodButton0 = document.getElementById('buyRod0');
-        fishingRodButton0.style.visibility = 'visible';
-
-        fishingRodHeading = document.getElementById('equipment-heading');
-        fishingRodHeading.style.visibility = 'visible';
-
-        fishingRodTradingDiv = document.getElementById('equipment-div');
-        fishingRodTradingDiv.style.removeProperty('border');
+        makeEquipmentTradingVisible();
+        makeRodVisible(0);
 
         fishingRods[0].unlocked = true;
     }
 
-    if(sandDollars > 40 && baits[0].unlocked && !fishHabitats[0].unlocked) { // unlocks habitat section of trading when 40 sanddollars are earned and bait is already unlocked
-        fishingHabitatTrading0 = document.getElementById('habitatTrading0-div');
-        fishingHabitatTrading0.style.visiblity = 'visible';
-
-        fishingHabitatButton0 = document.getElementById('buyHabitat0');
-        fishingHabitatButton0.style.visibility = 'visible';
-
-        habitatTradingHeading = document.getElementById('habitat-heading')
-        habitatTradingHeading.style.visibility = 'visible';
-
-        habitatTradingDiv = document.getElementById('habitat-trading-div');
-        habitatTradingDiv.style.removeProperty('border');
-
-        fishHabitats[0].unlocked = true;
+    if(sandDollars >= 40 && isBaitVisible(0) && !isHabitatVisible()) { // unlocks habitat section of trading when 40 sanddollars are earned and bait is already unlocked
+        makeHabitatTradingVisible();
+        makeHabitatVisible(0)
     }
 }
 
@@ -99,40 +150,17 @@ function buyRod(fishingRodNumber) {
         updateSandDollars();
 
         if (fishingRodNumber === 0) { // make supplies box and title visible
-            suppliesDiv = document.getElementById('supplies-div');
-            suppliesDiv.style.visibility = 'visible';
+            makeSuppliesVisible();
         } else if (fishingRodNumber === 1) { // the else ifs make the baits visible in inventory and trading section --> could maybe be classes. tried once, could try again
-            bait0Div = document.getElementById('bait0-div'); 
-            bait0Div.style.visibility = 'visible';
-
-            baitDiv = document.getElementById('baitTrading0-div');
-            baitDiv.style.visibility = 'visible';
-
-            baitHeadingDiv = document.getElementById('bait-heading');
-            baitHeadingDiv.style.visibility = 'visible';
-            
-            baitDiv = document.getElementById("bait-div");
-            baitDiv.style.removeProperty('border');
+            makeBaitTradingVisible();
+            makeBaitVisible(0);
 
             baits[0].unlocked = true; // to unlock the habitats section
         } else if (fishingRodNumber === 2) {
-            bait1Div = document.getElementById('bait1-div');
-            bait1Div.style.visibility = 'visible';
-
-            baitDiv = document.getElementById('baitTrading1-div');
-            baitDiv.style.visibility = 'visible';
-
-            bait2Div = document.getElementById('bait2-div');
-            bait2Div.style.visibility = 'visible';
-
-            baitDiv = document.getElementById('baitTrading2-div');
-            baitDiv.style.visibility = 'visible';
+            makeBaitVisible(1);
+            makeBaitVisible(2);
         } else if (fishingRodNumber === 3) {
-            bait3Div = document.getElementById('bait3-div');
-            bait3Div.style.visibility = 'visible';
-
-            baitDiv = document.getElementById('baitTrading3-div');
-            baitDiv.style.visibility = 'visible';
+            makeBaitVisible(3);
         }
 
         // toggling visibility as fishing rods are bought
@@ -140,10 +168,7 @@ function buyRod(fishingRodNumber) {
         if (fishingRodNumber != fishingRods.length - 1) {
             // make next rod visible
             let nextFishingRodNumber = fishingRodNumber + 1;
-            fishingRodTradingDiv = document.getElementById("fishingRodTrading" + nextFishingRodNumber + "-div");
-            fishingRodTradingDiv.style.visibility = 'visible';
-            nextFishingRodBuyButton = document.getElementById("buyRod" + nextFishingRodNumber);
-            nextFishingRodBuyButton.style.visibility = 'visible';
+            makeRodVisible(nextFishingRodNumber);
 
             // make current rod buy button invisible
             fishingRodBuyButton = document.getElementById("buyRod" + fishingRodNumber);
@@ -171,12 +196,13 @@ function buyHabitat(fishHabitatNumber) {
         // toggling visibility as habitats are bought
         console.log(fishHabitatNumber);
         if (fishHabitatNumber != fishHabitats.length - 1) {
+            if (fishHabitatNumber == 0) {
+                makeHabitatSectionVisible();
+            }
+
             // make next habitat visible
             let nextHabitatNumber = fishHabitatNumber + 1;
-            habitatTradingDiv = document.getElementById("habitatTrading" + nextHabitatNumber + "-div");
-            habitatTradingDiv.style.visibility = 'visible';
-            nextHabitatBuyButton = document.getElementById("buyHabitat" + nextHabitatNumber);
-            nextHabitatBuyButton.style.visibility = 'visible';
+            makeHabitatVisible(nextHabitatNumber);
 
             // make current habitat buy button invisible
             habitatBuyButton = document.getElementById("buyHabitat" + fishHabitatNumber);
@@ -193,15 +219,9 @@ function buyHabitat(fishHabitatNumber) {
         habitatMaximumSpan.innerText = currentHabitat.capacity;
 
         // changes displays based on what's unlocked --> shows all fish that have been unlocked before they bought the fish bowl
-        if (Object.keys(currentHabitat).length > 0) { // checks if the fish bowl has been bought
-            habitatDiv = document.getElementById('habitat-div'); // displays fish bowl
-            habitatDiv.style.visibility = 'visible';
-
-            for (fishNumber in fishStats) { // displays fish that are unlocked
-                if (fishStats[fishNumber].unlocked) {
-                    fishInHabitatDiv = document.getElementById("fishInHabitat" + fishNumber + "-div");
-                    fishInHabitatDiv.style.visibility = 'visible';
-                }
+        for (fishNumber in fishStats) { // displays fish that are unlocked
+            if (fishStats[fishNumber].unlocked) {
+                makeHabitatFishVisible(fishNumber);
             }
         }
     }
